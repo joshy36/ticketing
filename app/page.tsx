bad15aa715299f65025cc83c22197e5e13991af0
Joshua Bender <36bend@gmail.com>
2023-07-26 13:06:41 -0400

    tailwind working

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Events from './components/Events';
+import './globals.css';
+import Header from './components/Header';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <Events />
     </main>
   );
