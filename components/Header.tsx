69b152b6de3c3a8649adcf636a0f5b1719809d94
Joshua Bender <36bend@gmail.com>
2023-07-27 18:44:21 -0400

    event view page

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,6 +16,8 @@ import {
   PlayCircleIcon,
 } from '@heroicons/react/20/solid';
 
+import Link from 'next/link';
+
 const products = [
   {
     name: 'Analytics',
@@ -67,14 +69,14 @@ export default function Example() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -1,13 +1,11 @@
-import Events from '../../components/Events';
-import EventsBetter from '../../components/EventsBetter';
+import EventView from '../../components/EventView';
 import Header from '../../components/Header';
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <Events />
-      <EventsBetter />
+      <EventView />
     </main>
   );
 }
