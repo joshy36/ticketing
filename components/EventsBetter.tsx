dce8f22959788b3cf57564422d9bce747cfa9a9f
Joshua Bender <36bend@gmail.com>
2023-07-27 18:31:26 -0400

    404

--- components/EventsBetter.tsx
+++ components/EventsBetter.tsx
@@ -61,7 +61,7 @@ export default function Example() {
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+            <a key={product.id} href={`/event/${product.id}`} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <Image
                   src={product.imageSrc}
