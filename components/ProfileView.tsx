27c3739b7adb6d08ba6fff87cb049811904ac321
Joshua Bender <36bend@gmail.com>
2023-08-28 18:02:43 -0400

    edit profile button

--- components/ProfileView.tsx
+++ components/ProfileView.tsx
@@ -1,4 +1,5 @@
 import getUserProfile from '@/utils/getUserProfile';
+import createServerClient from '@/lib/supabaseServer';
 import { Button } from './ui/button';
 import Link from 'next/link';
 
@@ -7,7 +8,13 @@ export default async function ProfileView({
 }: {
   params: { id: string };
 }) {
-  const user: UserProfile = await getUserProfile(params.id);
+  const userProfile = await getUserProfile(params.id);
+
+  const supabase = createServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   return (
     <div className="bg-background">
@@ -29,7 +36,7 @@ export default async function ProfileView({
         <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 className="text-2xl font-bold tracking-tight text-accent-foreground sm:text-3xl">
-              {user.username}
+              {userProfile.username}
             </h1>
           </div>
 
@@ -37,7 +44,7 @@ export default async function ProfileView({
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             {/* <h2 className="sr-only">Product information</h2>
             <p className="text-3xl tracking-tight text-accent-foreground">{`$99999`}</p> */}
-            {user.id === params.id ? (
+            {user! && user.id === params.id ? (
               <form className="mt-10">
                 <Link href={`/user/edit/${params.id}`}>
                   <Button variant="default" className="flex w-full">
@@ -62,10 +69,12 @@ export default async function ProfileView({
             <div>
               <h3 className="sr-only">Description</h3>
               <div className="space-y-6">
-                <p className="text-base text-accent-foreground">{`${user.first_name} ${user.last_name}`}</p>
+                <p className="text-base text-accent-foreground">{`${userProfile.first_name} ${userProfile.last_name}`}</p>
               </div>
               <div className="space-y-6">
-                <p className="text-base text-accent-foreground">{user.bio}</p>
+                <p className="text-base text-accent-foreground">
+                  {userProfile.bio}
+                </p>
               </div>
             </div>
           </div>
