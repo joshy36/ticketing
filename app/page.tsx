a8e907d6b0cd5a081626e78548891d8e05c43c73
Joshua Bender <36bend@gmail.com>
2023-10-04 14:47:02 -0400

    text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <p className="py-20 px-10 text-center text-9xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+      <p className="py-20 px-10 text-center text-6xl md:text-9xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
         Welcome to the future of ticketing.
       </p>
     </main>
