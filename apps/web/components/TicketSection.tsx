a7b996e5a4d50ba08c6eb9309a5ebd2344972923
Joshua Bender <36bend@gmail.com>
2023-11-23 11:50:08 -0500

    move checkout

--- apps/web/components/TicketSection.tsx
+++ apps/web/components/TicketSection.tsx
@@ -8,6 +8,7 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import EventCheckout from './EventCheckout';
 import { UserProfile } from 'supabase';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 // need a better way to share this type between web and mobile
 export type Section = {
@@ -95,89 +96,111 @@ export default function TicketSection({
 
   return (
     <div>
-      {sections?.map((section) => (
-        <div key={section.id} className='flex flex-row justify-between py-2'>
-          <div className='flex flex-col'>
-            <div className='text-xl text-white'>{section.name}</div>
-            <div className='text text-gray-400'>
-              {`$` +
-                sectionPrices?.find(
-                  (sectionPrice) => sectionPrice.section_id === section.id,
-                )?.price}
-            </div>
-          </div>
-          <div className='flex flex-row items-center justify-center'>
-            <Button
-              variant='secondary'
-              size='icon'
-              onClick={() => {
-                handleRemoveTicket(section);
-              }}
-              disabled={
-                !ticketQuantities ||
-                // @ts-ignore
-                ticketQuantities[section.id!]?.quantity <= 0
-              }
-            >
-              <MinusCircledIcon />
-            </Button>
-            <div className='w-8 px-2 text-center text-white'>
-              {ticketQuantities[section.id!]?.quantity || 0}
-            </div>
-            <Button
-              variant='secondary'
-              size='icon'
-              onClick={() => {
-                handleAddTicket(section);
-              }}
-              disabled={getTotalTicketCount() >= event?.max_tickets_per_user!}
-            >
-              <PlusCircledIcon />
-            </Button>
-          </div>
-        </div>
-      ))}
-      {getTotalTicketCount() >= event?.max_tickets_per_user! ? (
-        <div>
-          <div className='text-center text-white'>Max 2 tickets per user</div>
-        </div>
-      ) : (
-        <div></div>
-      )}
-      <Separator />
-      <div className='py-4'>
-        <div className='text-xl text-white'>Total: ${getTotalPrice()}</div>
-      </div>
-      {userProfile ? (
-        <Button
-          variant='default'
-          className='flex w-full'
-          disabled={getTotalTicketCount() == 0}
-          onClick={() => {
-            setCheckout(true);
-          }}
-        >
-          {getTotalTicketCount() === 0
-            ? 'Add tickets to cart'
-            : 'Proceed to Checkout'}
-        </Button>
-      ) : (
-        // </Link>
-        <Link href={`/sign-in`}>
-          <Button variant='default' className='flex w-full'>
-            Sign in to Purchase
-          </Button>
-        </Link>
-      )}
       {checkout ? (
-        <EventCheckout
-          event={event!}
-          userProfile={userProfile!}
-          ticketQuantities={Object.values(ticketQuantities)}
-          totalPrice={getTotalPrice()}
-        />
+        <div>
+          <Button
+            variant='outline'
+            className='pl-1'
+            onClick={() => setCheckout(false)}
+          >
+            <ChevronLeft />
+            Back
+          </Button>
+          <EventCheckout
+            event={event!}
+            userProfile={userProfile!}
+            ticketQuantities={Object.values(ticketQuantities)}
+            totalPrice={getTotalPrice()}
+          />
+        </div>
       ) : (
-        <div></div>
+        <div>
+          {sections?.map((section) => (
+            <div
+              key={section.id}
+              className='flex flex-row justify-between py-2'
+            >
+              <div className='flex flex-col'>
+                <div className='text-xl text-white'>{section.name}</div>
+                <div className='text text-gray-400'>
+                  {`$` +
+                    sectionPrices?.find(
+                      (sectionPrice) => sectionPrice.section_id === section.id,
+                    )?.price}
+                </div>
+              </div>
+              <div className='flex flex-row items-center justify-center'>
+                <Button
+                  variant='secondary'
+                  size='icon'
+                  onClick={() => {
+                    handleRemoveTicket(section);
+                  }}
+                  disabled={
+                    !ticketQuantities ||
+                    // @ts-ignore
+                    ticketQuantities[section.id!]?.quantity <= 0
+                  }
+                >
+                  <MinusCircledIcon />
+                </Button>
+                <div className='w-8 px-2 text-center text-white'>
+                  {ticketQuantities[section.id!]?.quantity || 0}
+                </div>
+                <Button
+                  variant='secondary'
+                  size='icon'
+                  onClick={() => {
+                    handleAddTicket(section);
+                  }}
+                  disabled={
+                    getTotalTicketCount() >= event?.max_tickets_per_user!
+                  }
+                >
+                  <PlusCircledIcon />
+                </Button>
+              </div>
+            </div>
+          ))}
+          {getTotalTicketCount() >= event?.max_tickets_per_user! ? (
+            <div>
+              <div className='text-center text-white'>
+                Max 2 tickets per user
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <Separator />
+          <div className='py-4'>
+            <div className='text-xl text-white'>Total: ${getTotalPrice()}</div>
+          </div>
+          {userProfile ? (
+            <Button
+              variant='default'
+              className='flex w-full'
+              disabled={getTotalTicketCount() == 0}
+              onClick={() => {
+                setCheckout(true);
+              }}
+            >
+              {getTotalTicketCount() === 0 ? (
+                'Add tickets to cart'
+              ) : (
+                <div className='flex flex-row items-center'>
+                  <div>Proceed to Checkout</div>
+                  <ChevronRight />
+                </div>
+              )}
+            </Button>
+          ) : (
+            <Link href={`/sign-in`}>
+              <Button variant='default' className='flex w-full'>
+                Sign in to Purchase
+              </Button>
+            </Link>
+          )}
+        </div>
       )}
     </div>
   );
