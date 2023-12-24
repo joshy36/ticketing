b77e2b4fcd3b13bc3a5c4675e60ae585edb439d7
Joshua Bender <36bend@gmail.com>
2023-12-23 21:15:41 -0500

    fixing buton

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,41 +4,39 @@ import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 
 // https://github.com/adueck/pwa-install-demo/blob/badb37b641fb115867fd0b90d54d1cd91eaf22b6/src/App.js#L11
-let deferredPrompt: any;
 
 export default function Home() {
   const [installable, setInstallable] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState<Event>();
 
   useEffect(() => {
     window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent the mini-infobar from appearing on mobile
       e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI notify the user they can install the PWA
+      setDeferredPrompt(e);
       setInstallable(true);
     });
 
     window.addEventListener('appinstalled', () => {
-      // Log install to analytics
       console.log('INSTALL: Success');
     });
   }, []);
 
   const handleInstallClick = (e: any) => {
-    // Hide the app provided install promotion
     setInstallable(false);
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult: any) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-    });
+    if (deferredPrompt) {
+      // @ts-ignore
+      deferredPrompt.prompt();
+      // @ts-ignore
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
+    }
   };
+
   return (
     <main>
       {installable && (
