ebe956b2026610936dfef7c712dcaebad1109c8a
Joshua Bender <36bend@gmail.com>
2023-12-23 21:01:10 -0500

    install button

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,50 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useEffect, useState } from 'react';
+
+let deferredPrompt: any;
+
 export default function Home() {
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener('appinstalled', () => {
+      // Log install to analytics
+      console.log('INSTALL: Success');
+    });
+  }, []);
+
+  const handleInstallClick = (e) => {
+    // Hide the app provided install promotion
+    setInstallable(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  };
   return (
     <main>
+      {installable && (
+        <Button className='w-full rounded-none' onClick={handleInstallClick}>
+          Install App For Native Experience
+        </Button>
+      )}
       <p className='bg-gradient-to-r from-white to-gray-500 bg-clip-text px-20 py-20 text-center text-4xl font-bold tracking-tighter text-transparent md:text-9xl'>
         Welcome to the future of ticketing.
       </p>
