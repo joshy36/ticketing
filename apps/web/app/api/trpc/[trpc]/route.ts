5c2fdd17d89cd9d9e610d09bf06a2e27974cfbec
Joshua Bender <36bend@gmail.com>
2024-02-02 00:21:00 -0500

    anotha one

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -12,11 +12,9 @@ const handler = (req: NextRequest) =>
     responseMeta() {
       return {
         headers: {
-          'Access-Control-Allow-Credentials': 'true',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          'Access-Control-Allow-Headers':
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          'Access-Control-Allow-Headers': '*',
         },
       };
     },
