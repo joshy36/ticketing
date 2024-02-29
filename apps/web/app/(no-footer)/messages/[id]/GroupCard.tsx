4bd63a39f51bd3ae3622f2bfea54cddfa7a3a3d6
Joshua Bender <36bend@gmail.com>
2024-02-29 13:35:04 -0500

    key

--- apps/web/app/(no-footer)/messages/[id]/GroupCard.tsx
+++ apps/web/app/(no-footer)/messages/[id]/GroupCard.tsx
@@ -23,7 +23,7 @@ export default function GroupCard({
     <div className='flex items-center gap-2'>
       <div className='flex items-end -space-x-6 rtl:space-x-reverse'>
         {otherMembers.map((member) => (
-          <Avatar className='z-40 opacity-100'>
+          <Avatar className='z-40 opacity-100' key={member.id}>
             {userProfile?.profile_image ? (
               <AvatarImage src={member?.profile_image!} alt='pfp' />
             ) : (
