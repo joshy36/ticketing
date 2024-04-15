b87f7f4a1c6b5ba679b62299ee3e28b01e641500
Joshua Bender <36bend@gmail.com>
2024-04-15 14:57:05 -0400

    testing

--- apps/web/inngest/functions.ts
+++ apps/web/inngest/functions.ts
@@ -1,17 +1,20 @@
-import { serverClient } from '@/app/_trpc/serverClient';
 import { inngest } from './client';
+import { ethers } from 'ethers';
+import contractAbi from '../../../packages/chain/deployments/base-goerli/Event.json';
+import createSupabaseServer from '@/utils/supabaseServer';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world', concurrency: 1 },
   { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    // await step.sleep('wait-a-moment', '10s');
-    await step.run('simulate work', async () => {
-      // use await to sleep for 10 s
-      console.log('Sleeping for 10s');
-      await new Promise((resolve) => setTimeout(resolve, 10000));
-      console.log('Done sleeping');
-    });
+  async ({ event }) => {
+    console.log('Sleeping for 10s');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log('Done sleeping');
     return { event, body: 'Hello, World!' };
   },
 );
@@ -19,20 +22,63 @@ export const helloWorld = inngest.createFunction(
 export const transferTicket = inngest.createFunction(
   { id: 'transfer-ticket', concurrency: 1 },
   { event: 'ticket/transfer' },
-  async ({ event, step }) => {
+  async ({ event }) => {
     console.log(`INNGEST::recieved: ${event.data.ticket_id}`);
-    await step.run('transfer', async () => {
-      try {
-        const tx = await serverClient.transferTicket.mutate({
-          event_id: event.data.event_id,
-          ticket_id: event.data.ticket_id,
-          user_id: event.data.user_id,
-        });
-        return { event, body: `tx: ${tx}` };
-      } catch (err) {
-        console.log('Transaction failed: ', err);
-        throw new Error(`Transaction failed: ${err}`);
-      }
-    });
+    const supabase = createSupabaseServer();
+    const { data: eventSupabase } = await supabase
+      .from('events')
+      .select()
+      .eq('id', event.data.event_id)
+      .limit(1)
+      .single();
+
+    const { data: ticket, error: ticketError } = await supabase
+      .from('tickets')
+      .select()
+      .eq('id', event.data.ticket_id)
+      .limit(1)
+      .single();
+
+    const { data: userProfile } = await supabase
+      .from('user_profiles')
+      .select()
+      .eq('id', event.data.user_id)
+      .limit(1)
+      .single();
+
+    if (ticketError?.code == 'PGRST116') {
+      throw new Error('Ticket not found');
+    }
+
+    const link = eventSupabase?.etherscan_link?.split('/');
+    if (!link) {
+      throw new Error('No etherscan link found for event');
+    }
+
+    const address = link[link.length - 1]!;
+    const provider = new ethers.JsonRpcProvider(
+      process.env.ALCHEMY_SEPOLIA_URL!,
+    );
+
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+    const eventContract = new ethers.Contract(address, contractAbi.abi, signer);
+
+    // @ts-ignore
+    let tx = await eventContract.safeTransferFrom(
+      signer.address,
+      userProfile?.wallet_address,
+      ticket?.token_id,
+    );
+
+    console.log(
+      `Token transferred! Check it out at: https://base-sepolia.blockscout.com/tx/${tx.hash}`,
+    );
+
+    await supabase
+      .from('tickets')
+      .update({
+        current_wallet_address: userProfile?.wallet_address,
+      })
+      .eq('id', event.data.ticket_id);
   },
 );
