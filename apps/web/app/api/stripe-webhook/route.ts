50053879faf2ac6213dceb1e8607c3e5ef438940
Joshua Bender <36bend@gmail.com>
2024-04-22 12:53:51 -0400

    tsconfig

--- apps/web/app/api/stripe-webhook/route.ts
+++ apps/web/app/api/stripe-webhook/route.ts
@@ -3,9 +3,8 @@ import Stripe from 'stripe';
 import { createRouteClient } from 'supabase';
 import { inngest } from '@/inngest/client';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
 export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
   const payload = await req.text();
   const sig = req.headers.get('stripe-signature') as string;
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
