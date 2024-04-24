97f949e03b9b38dcf7b84c704bb31a7ea9c75134
Joshua Bender <36bend@gmail.com>
2024-04-24 17:20:46 -0400

    remove upstash

--- apps/web/README.md
+++ apps/web/README.md
@@ -19,16 +19,6 @@ npx supabase start
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-## Qstash Queue
-
-[Local Tunnel](https://upstash.com/docs/qstash/howto/local-tunnel)
-
-In a new terminal run:
-
-```bash
-ngrok http --domain=internally-internal-walleye.ngrok-free.app 3000
-```
-
 ## Tech Stack
 
 Framework - [Nextjs](https://nextjs.org/)
--- packages/api/src/routers/openai.ts
+++ packages/api/src/routers/openai.ts
@@ -12,9 +12,6 @@ export const openAiRouter = router({
       const prompt =
         input.prompt.replace(/_/g, ' ') +
         ' in space retro theme profile picture';
-      // const openaiApiKey = process.env.OPEN_AI_API_KEY;
-
-      console.log('generating image from openai');
 
       await inngest.send({
         name: 'user/generate.pfp',
@@ -23,27 +20,5 @@ export const openAiRouter = router({
           id: input.id,
         },
       });
-
-      // const ticket = await inngest.send({
-      //   name: 'user/generate-pfp',
-      //   data: {
-      //     prompt: prompt,
-      //     id: input.id,
-      //   },
-      // });
-
-      // const res = await qstashClient.publishJSON({
-      //   url: 'https://api.openai.com/v1/images/generations',
-      //   callback: `${process.env.UPSTASH_URL}/api/qstash-callback-dalle?id=${input.id}`,
-      //   headers: {
-      //     'upstash-forward-Authorization': `Bearer ${openaiApiKey}`,
-      //   },
-      //   body: {
-      //     model: 'dall-e-3',
-      //     prompt: prompt,
-      //     n: 1,
-      //     size: '1024x1024',
-      //   },
-      // });
     }),
 });
