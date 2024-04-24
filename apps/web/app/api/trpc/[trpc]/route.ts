413a1a3cabc15a6146adf495a9cd4d3e9681e9f3
Joshua Bender <36bend@gmail.com>
2024-04-24 14:52:05 -0400

    max dur2

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -3,7 +3,9 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { createTRPCContext } from 'api/src/trpc';
 import type { NextRequest } from 'next/server';
 
-export const maxDuration = 300; // This function can run for a maximum of 5 seconds
+export const config = {
+  maxDuration: 300,
+};
 
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
