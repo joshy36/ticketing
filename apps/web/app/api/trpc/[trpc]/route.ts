85d0a95da4c86e7dfbf7dcad7db93f5968e2b56b
Joshua Bender <36bend@gmail.com>
2024-04-24 14:59:52 -0400

    vercel

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -3,10 +3,6 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { createTRPCContext } from 'api/src/trpc';
 import type { NextRequest } from 'next/server';
 
-export const config = {
-  maxDuration: 300,
-};
-
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
