3c40ef13c43b1f2a6d7109f9ff31eaf8dceb2102
Joshua Bender <36bend@gmail.com>
2024-04-25 17:24:24 -0400

    better size

--- apps/web/app/(footer)/[username]/ProfileView.tsx
+++ apps/web/app/(footer)/[username]/ProfileView.tsx
@@ -90,11 +90,11 @@ export default async function ProfileView({
             )}
           </div>
           <div className='flex flex-row'>
-            <p className='md:text-md pb-8 text-sm font-light text-muted-foreground'>{`@${userProfile?.username} ·`}</p>
-            <p className='md:text-md ml-1 pb-8 text-sm font-semibold text-muted-foreground hover:underline'>{`${friendCount} friends`}</p>
+            <p className='md:text-md pb-6 text-sm font-light text-muted-foreground'>{`@${userProfile?.username} ·`}</p>
+            <p className='md:text-md ml-1 pb-6 text-sm font-semibold text-muted-foreground hover:underline'>{`${friendCount} friends`}</p>
           </div>
           {userProfile?.bio && (
-            <p className='text-lg font-light'>{`${userProfile?.bio}`}</p>
+            <p className='font-light'>{`${userProfile?.bio}`}</p>
           )}
           {session?.user! && session?.user.id !== userProfile?.id && (
             <FriendRequest
