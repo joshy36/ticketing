e0cc8081ff2ed96b53fbccb5bf1d966c337b46df
Joshua Bender <36bend@gmail.com>
2024-06-14 14:21:36 -0400

    fixed?

---
+++ apps/web/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gclzfhnchcgtgcmzpvna.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/images/**',
+      },
+    ],
+    domains: ['gclzfhnchcgtgcmzpvna.supabase.co', 'localhost'],
+  },
+  transpilePackages: ['api', 'supabase'],
+  // logging: {
+  //   fetches: {
+  //     fullUrl: true,
+  //   },
+  // },
+};
