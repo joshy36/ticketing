441d1982393ed6138e431e88e46e72e49c07a4d2
Joshua Bender <36bend@gmail.com>
2024-06-14 13:52:02 -0400

    fix redirect

--- apps/web/app/(footer)/demo/page.tsx
+++ apps/web/app/(footer)/demo/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('https://www.google.com');
+  redirect('/');
 
   return (
     <div className='flex flex-col items-center justify-center py-20'>demo</div>
