6c92a4b7e044173cb6d96dcdf5061fc9fab5e978
Joshua Bender <36bend@gmail.com>
2024-06-25 15:08:07 -0400

    realtime collectibles

--- apps/dashboard/app/(sidebar)/[organization_id]/event/[event_id]/CreateCollectibles.tsx
+++ apps/dashboard/app/(sidebar)/[organization_id]/event/[event_id]/CreateCollectibles.tsx
@@ -13,11 +13,14 @@ import { Skeleton } from '~/components/ui/skeleton';
 import { Input } from '~/components/ui/input';
 import { Button } from '~/components/ui/button';
 import { Image as ImageIcon, RefreshCcw } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Icons } from '~/components/ui/icons';
 import Image from 'next/image';
+import createSupabaseBrowserClient from '~/utils/supabaseBrowser';
 
 export default function CreateCollectibles({ event }: { event: Events }) {
+  const supabase = createSupabaseBrowserClient();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [prompt, setPrompt] = useState<string>('');
 
@@ -26,17 +29,48 @@ export default function CreateCollectibles({ event }: { event: Events }) {
   };
 
   const { data: collectibles, refetch: refetchCollectibles } =
-    trpc.getCollectiblesForEvent.useQuery({ event_id: event.id });
+    trpc.getCollectiblesForEvent.useQuery({
+      event_id: event.id,
+    });
 
-  const { data: sbts, refetch: refetchSbts } = trpc.getSbtsForEvent.useQuery({
-    event_id: event.id,
-  });
+  const sortedCollectibles = collectibles
+    ?.filter((collectible) => collectible.image != null)
+    .sort((a, b) => {
+      const tokenA = a.tickets?.token_id;
+      const tokenB = b.tickets?.token_id;
+      if (tokenA && tokenB) {
+        return tokenA - tokenB;
+      }
+      return 0;
+    });
 
   const generateImages = trpc.generateEventImages.useMutation({
     onSettled(data, error, variables, context) {
       setIsLoading(false);
     },
   });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('test-channel')
+      .on(
+        'postgres_changes',
+        {
+          event: '*',
+          schema: 'public',
+          table: 'collectibles',
+          filter: `event_id=eq.${event?.id}`,
+        },
+        (payload) => {
+          refetchCollectibles();
+        },
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase]);
 
   return (
     <Card className='mt-4 w-full rounded-md border bg-zinc-950'>
@@ -59,6 +93,10 @@ export default function CreateCollectibles({ event }: { event: Events }) {
               setIsLoading(true);
               generateImages.mutate({ event_id: event.id, prompt: prompt });
             }}
+            disabled={
+              isLoading ||
+              collectibles?.find((c) => c.image != null) != undefined
+            }
           >
             {isLoading && (
               <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
@@ -67,53 +105,25 @@ export default function CreateCollectibles({ event }: { event: Events }) {
             Generate Images
           </Button>
         </div>
-        <h2 className='py-4 text-lg font-bold'>SBTs</h2>
-        <Button
-          // onClick={() => refetchSbts()}
-          className='mb-4 rounded-md'
-          variant='secondary'
-        >
-          <RefreshCcw className='pr-2' />
-          Refresh
-        </Button>
-        <div className='grid grid-cols-4 gap-4'>
-          {sbts?.map((sbt) => (
-            <div key={sbt.id} className='flex justify-center'>
-              <div className='relative'>
-                <Image
-                  src={sbt.image || event.image!}
-                  alt='image'
-                  width={200}
-                  height={200}
-                  className='aspect-square rounded-lg border'
-                />
-                <div className='absolute bottom-0 left-0 right-0 rounded-bl-lg rounded-br-lg bg-zinc-900 bg-opacity-50 backdrop-blur-md'>
-                  <h1 className='py-4 pl-4 text-sm font-light text-accent-foreground'>
-                    {sbt?.tickets?.token_id}
-                  </h1>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <h2 className='py-4 text-lg font-bold'>Collectibles</h2>
-        <div className='grid grid-cols-4 gap-4 pt-4'>
-          {collectibles?.map((collectible) => (
+        <div className='grid grid-cols-4 gap-4 pt-8'>
+          {sortedCollectibles?.map((collectible) => (
             <div key={collectible.id} className='flex justify-center'>
-              <div>
-                <Image
-                  src={collectible.image || event.image!}
-                  alt='image'
-                  width={200}
-                  height={200}
-                  className='aspect-square rounded-tl-lg rounded-tr-lg'
-                />
-                <div className='rounded-bl-lg rounded-br-lg bg-zinc-900'>
-                  <h1 className='py-2 pl-4 text-accent-foreground'>
-                    {collectible?.tickets?.token_id}
-                  </h1>
+              {collectible.image && (
+                <div className='relative'>
+                  <Image
+                    src={collectible.image}
+                    alt='image'
+                    width={200}
+                    height={200}
+                    className='aspect-square rounded-lg border'
+                  />
+                  <div className='absolute bottom-0 left-0 right-0 rounded-bl-lg rounded-br-lg bg-zinc-900 bg-opacity-50 backdrop-blur-md'>
+                    <h1 className='py-4 pl-4 text-sm font-light text-accent-foreground'>
+                      {collectible?.tickets?.token_id}
+                    </h1>
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
           ))}
         </div>
