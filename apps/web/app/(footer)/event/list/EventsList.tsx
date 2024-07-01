f40ee40a27d53074310ec5994f16c59dd4817f57
Joshua Bender <36bend@gmail.com>
2024-07-01 11:16:51 -0400

    ui

--- apps/web/app/(footer)/event/list/EventsList.tsx
+++ apps/web/app/(footer)/event/list/EventsList.tsx
@@ -3,6 +3,8 @@
 import { dateToString } from '~/utils/helpers';
 import { RouterOutputs } from 'api';
 import Image from 'next/image';
+import { Card, CardContent } from '~/components/ui/card';
+import Link from 'next/link';
 
 export default function EventsList({
   events,
@@ -10,53 +12,37 @@ export default function EventsList({
   events: RouterOutputs['getEvents'];
 }) {
   return (
-    <div className="-mt-16 min-h-screen bg-[url('/img5-70.png')] bg-cover bg-center">
-      <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 '>
-        <h1 className='pb-8 pt-24 text-4xl'>Explore Upcoming Events</h1>
-        <div className='grid grid-cols-1 gap-y-2'>
-          {events?.map((event: any) => (
-            <a
-              key={event.id}
-              href={`/event/${event.id}`}
-              className='group flex flex-row gap-6 rounded-3xl border bg-black/50 p-6 backdrop-blur-md hover:bg-black/60'
-            >
-              <div className='xl:aspect-h-8 xl:aspect-w-7 aspect-square w-24 overflow-hidden rounded-lg bg-background'>
-                {event.image ? (
-                  <Image
-                    src={event.image}
-                    alt={event.description}
-                    width={500}
-                    height={500}
-                    className='h-full w-full object-cover object-center transition group-hover:opacity-75'
-                  />
-                ) : (
-                  <Image
-                    src='/fallback.jpeg'
-                    alt='image'
-                    width={500}
-                    height={500}
-                    className='h-full w-full object-cover object-center group-hover:opacity-75'
-                  />
-                )}
-              </div>
-              <div>
-                <h1 className='text-xl font-medium text-accent-foreground'>
-                  {event.name}
-                </h1>
-                <p className='mt-0.5 text-sm text-muted-foreground'>
-                  {`${dateToString(event.date)}`}
-                </p>
-                <p className='mt-0.5 text-sm text-muted-foreground'>
-                  {`${event.artists.name}`}
-                </p>
-                <p className='mt-0.5 text-sm text-muted-foreground'>
-                  {`${event.venues.name}`}
-                </p>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
+    <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 '>
+      <h1 className='pb-8 pt-12 text-4xl'>Explore Upcoming Events</h1>
+
+      <section className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {events?.map((event) => (
+          <Link href={`/event/${event.id}`} key={event.id}>
+            <div>
+              <Card className='group overflow-hidden border'>
+                <Image
+                  src={event.image!}
+                  alt='Event Image'
+                  width={400}
+                  height={300}
+                  className='aspect-[4/3] w-full object-cover transition-transform group-hover:scale-105'
+                />
+                <CardContent className='bg-zinc-950 p-4'>
+                  <h3 className='text-lg font-semibold'>{event.name}</h3>
+                  <div className='flex items-center gap-2 text-sm text-muted-foreground'>
+                    <span>{`${dateToString(event.date)}`}</span>
+                    <span>·</span>
+                    <span> {`${event.artists?.name}`}</span>
+                  </div>
+                  <p className='text-sm text-muted-foreground'>
+                    {`${event.venues?.name}`}
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 }
